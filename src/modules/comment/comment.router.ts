f4bdf465c58e3050b52e889d6df4e6f29878eb15
Himon <mdhimonmia64@gmail.com>
2026-08-26 23:29:11 +0600

    Create Comment

--- src/modules/comment/comment.router.ts
+++ src/modules/comment/comment.router.ts
@@ -1,6 +1,9 @@
 import express, {Router} from 'express';
+import { CommentController } from './comment.controller';
+import auth, { UserRole } from '../../middleware/auth';
 
 const router = express.Router();
 
+router.post('/',auth(UserRole.USER,UserRole.ADMIN),CommentController.createComment)
 
-export const commentRouter:Router = router;
+export const commentRouter:Router = router;        
